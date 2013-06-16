63965208b5437ae1bc716a59a6d15099b17755be
gitprofit <liszcz.michal@gmail.com>
2013-06-16 03:23:50 +0200

    q

--- HttpServer/src/RequestHandler.hpp
+++ HttpServer/src/RequestHandler.hpp
@@ -67,7 +67,7 @@ public:
 
 		try
 		{
-			auto requestedFile = fileManager->getFile((*request)["URI"]);
+			auto requestedFile = fileManager->getFile((*request)["UR"]);
 			auto response = responseFactory->fromFile(Net::HttpStatusCode::OK, requestedFile);
 			socket->write(response);
 			socket->close();
